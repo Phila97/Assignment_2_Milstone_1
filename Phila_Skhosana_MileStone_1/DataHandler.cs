b65ffcfe6f5685ef13176d952b8280f97685d2ae
TyriqueVanwyk <you@example.com>
2021-11-01 17:48:38 +0200

    added System.Data.SqlClient and created connection in Datahandler class

--- Phila_Skhosana_MileStone_1/DataHandler.cs
+++ Phila_Skhosana_MileStone_1/DataHandler.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace Phila_Skhosana_MileStone_1
 {
     class DataHandler
     {
+        public SqlConnection conn = new SqlConnection(@"Data Source = (local); Initial Catalog=BelgiumCampus_Students; Integrated Security=SSPI");
+        public DataHandler()
+        {
+
+        }
     }
 }
