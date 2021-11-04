d59a4d5939f1e5b7cd232d7a074bc819c0a1e742
TyriqueVanwyk <you@example.com>
2021-11-04 23:05:54 +0200

    New script

---
+++ PRG252_Assignement2/DataHandler.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Phila_Skhosana_MileStone_1
+{
+    class DataHandler
+    {
+        public SqlConnection conn = new SqlConnection(@"Data Source = (local); Initial Catalog=BelgiumCampus_Students; Integrated Security=SSPI");
+        public DataHandler()
+        {
+            
+        }
+
+        public DataSet ShowData()
+        {
+            string sql = "SELECT * FROM tblStudents";
+            SqlDataAdapter data_adapter = new SqlDataAdapter(sql, conn);
+            DataSet ds = new DataSet();
+            conn.Open();
+            data_adapter.Fill(ds, "tblStudents");
+            conn.Close();
+            return ds;
+        }
+
+        //Search Method
+        public DataSet Search(string search)
+        {
+            try
+            {
+                string sql = $"SELECT * FROM tblStudents WHERE StudentNumber={search}";
+                SqlDataAdapter data_adapter = new SqlDataAdapter(sql, conn);
+                DataSet ds = new DataSet();
+                conn.Open();
+                data_adapter.Fill(ds, "tblStudents");
+                conn.Close();
+                return ds;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine($"Error: {exc}");
+                throw;
+            }
+            
+        }
+
+        //Insert Method
+        public void Insert(string first_name, string last_name, string gender, string phone, string address, string module_code)
+        {
+            try
+            {
+                //Open the connection
+                conn.Open();
+                Console.WriteLine("Connection Open");
+
+                //Query the DB To insert
+                SqlDataAdapter cmd = new SqlDataAdapter();
+                cmd.InsertCommand = new SqlCommand($"INSERT INTO tblStudents (FirstName, LastName, Gender, Phone, Address, Address, ModulesCodes) VALUES({first_name}, {last_name}, {gender}, {phone}, {address}, {module_code})");
+                cmd.InsertCommand.Connection = conn;
+
+                Console.WriteLine("Student Successfully added");
+                conn.Close();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine($"Error: {exc}");
+                throw;
+            }
+        }
+
+        //Delete Method
+        public void Delete(string search)
+        {
+            try
+            {
+                //Open the connection
+                conn.Open();
+                Console.WriteLine("Connection open");
+
+                //Query to delete
+                SqlDataAdapter cmd = new SqlDataAdapter();
+                cmd.DeleteCommand = new SqlCommand($"DELETE FROM tblStudents WHERE StudentNumber = {search}");
+                cmd.DeleteCommand.Connection = conn;
+
+                Console.WriteLine("Item Deleted");
+                //Close connection
+                conn.Close();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine($"Error{exc}");
+                throw;
+            }
+            
+        }
+
+        //Update function
+        public void Update(string search, string first_name, string last_name, string gender, string phone, string address, string module_code)
+        {
+            try
+            {
+                //Open connection
+                conn.Open();
+                Console.WriteLine("Connection open");
+
+                //Update query
+                SqlDataAdapter cmd = new SqlDataAdapter();
+                cmd.UpdateCommand = new SqlCommand($"UPDATE tblStudents SET FirstName = {first_name}, LastName = {last_name}, Gender = {gender}, Phone = {phone}, Address = {address}, ModulesCodes = {module_code} WHERE StudentNumber = {search}");
+                cmd.UpdateCommand.Connection = conn;
+
+                Console.Write("Update was successfull");
+            }
+            catch (Exception exc)
+            {
+                Console.Write($"Err {exc}");
+                throw;
+            }
+        }
+    }
+}
---
+++ PRG252_Assignement2/Details.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phila_Skhosana_MileStone_1
+{
+    class Details
+    {
+        private int studentNumber;
+
+        public int StudentNumber
+        {
+            get { return studentNumber; }
+            set { studentNumber = value; }
+        }
+
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string surname;
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        private string dob;
+
+        public string DOB
+        {
+            get { return dob; }
+            set { dob = value; }
+        }
+
+        private string gender;
+
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        private int phone;
+
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        private string  address;
+
+        public string  Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        private string moduleCode;
+
+        public string ModuleCode
+        {
+            get { return moduleCode; }
+            set { moduleCode = value; }
+        }
+
+        private string moduleName;
+
+        public string ModuleName
+        {
+            get { return moduleName; }
+            set { moduleName = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private string link;
+
+        public string Link
+        {
+            get { return link; }
+            set { link = value; }
+        }
+
+
+        public Details(int studentNumber, string name,string surname, string dob, string gender, int phone, string address,string moduleCode, string moduleName, string description, string link)
+        {
+            this.studentNumber = studentNumber;
+            this.surname = surname;
+            this.name = name;
+            this.dob = dob;
+            this.gender = gender;
+            this.phone = phone;
+            this.address = address;
+            this.moduleCode = moduleCode;
+            this.moduleName =moduleName;
+            this.description = description;
+            this.link = link;
+        }
+
+        public override string ToString()
+        {
+            return studentNumber + name + surname + dob + gender + phone + address + moduleCode + moduleName+ description + link ;
+
+        }
+    }
+}
